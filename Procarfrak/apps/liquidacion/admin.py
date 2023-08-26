6276bb3d6b87487b088480ec482d3451e37a508e
Pablo628 <pabloandresmen2002@gmail.com>
2023-08-26 10:17:09 -0500

    Proyecto Carfrakma

---
+++ Procarfrak/apps/liquidacion/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.liquidacion.models import EstadoL, Liquidacion
+
+# Register your models here.
+admin.site.register(EstadoL)
+admin.site.register(Liquidacion)
