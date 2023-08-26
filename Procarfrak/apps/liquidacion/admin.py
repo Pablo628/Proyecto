e3dbd207ab141e0a7d091d5f35ff8bc28a7f9ddb
Pablo628 <pabloandresmen2002@gmail.com>
2023-08-26 10:24:59 -0500

    eliminar

--- Procarfrak/apps/liquidacion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.liquidacion.models import EstadoL, Liquidacion
-
-# Register your models here.
-admin.site.register(EstadoL)
-admin.site.register(Liquidacion)
