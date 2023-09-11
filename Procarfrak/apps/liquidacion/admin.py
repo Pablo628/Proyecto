e7c1e84ea8f4f94bfd9142438789d516283726eb
Pablo628 <pabloandresmen2002@gmail.com>
2023-09-11 08:54:21 -0500

    e4

--- Procarfrak/apps/liquidacion/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from apps.liquidacion.models import  Liquidacion
-
-# Register your models here.
-admin.site.register(Liquidacion)
--- Procarfrak/apps/usuario/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from apps.usuario.views import *
-
-
-urlpatterns = [
-    re_path(r'^nuevo$', RegistroUsuario.as_view(), name='Registro'),
-]
