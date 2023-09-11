4f85e587434df5bac21a40744b05e238735ac2fb
Pablo628 <pabloandresmen2002@gmail.com>
2023-09-11 06:53:30 -0500

    e

--- Procarfrak/apps/vehiculo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from apps.vehiculo.models import Vehiculo, EstadoV
-# from apps.mascota.apps import Vacuna
-
-# Register your models here.
-admin.site.register(EstadoV)
-admin.site.register(Vehiculo)
-
